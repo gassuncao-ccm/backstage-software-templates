29533e364052faba98df5771b056043751d75982
gassuncao-ccm <gassuncao@ccmtecnologia.com.br>
2026-03-09 20:45:31 +0000

    Adds python app software template

---
+++ python-app/template/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human!! O que sobra?? Alguém calcula? Nem as migalhas! Nem ao menos as migalhas!!',
+        'deployed_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=5532)
